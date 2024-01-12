c944751f3fffa4e244731c7902ba512ab1110577
Jeongseunghun <seunghun311@naver.com>
2024-01-12 20:36:11 +0900

    DOMtree 생성

--- DOMtree.js
+++ DOMtree.js
@@ -1,40 +1,41 @@
-//자바스크립트의 실행컨텍스트와 스코프에 대한 html 파일을 string으로 입력받아 HTML 의 DOM tree를 생성하는 함수를 작성
+// //자바스크립트의 실행컨텍스트와 스코프에 대한 html 파일을 string으로 입력받아 HTML 의 DOM tree를 생성하는 함수를 작성
 
-function createDOMFromString(htmlString) {
-  var wrapper = document.createElement('div');
-  wrapper.innerHTML = htmlString;
+function parseHTMLString(htmlString) {
+  const domTree = {
+    tagName: 'document',
+    children: [],
+  };
 
-  var scriptElements = wrapper.querySelectorAll('script');
-  
-  scriptElements.forEach(function(script) {
-    var code = script.textContent || script.innerText;
-    eval(code);
-  });
+  let currentNode = domTree;
 
-  return wrapper;
+  const tagRegex = /<\s*([a-zA-Z0-9-]+)[^>]*>/g;
+  let matches;
+  while ((matches = tagRegex.exec(htmlString)) !== null) {
+    // matches[1]: 태그 이름
+    const tagName = matches[1].toLowerCase();
+
+    const newNode = {
+      tagName,
+      children: [],
+    };
+
+    currentNode.children.push(newNode);
+
+    // 새로운 노드가 자식을 가질 수 있는 경우, 현재 노드를 갱신
+    if (!isVoidElement(tagName)) {
+      currentNode = newNode;
+    }
+  }
+
+  return domTree;
 }
 
-async function readHTMLFile(fileName) {
-  try {
-    var response = await fetch(fileName);
-    var htmlString = await response.text();
-    return htmlString;
-  } catch (error) {
-    console.error('Error reading HTML file:', error);
-    return null;
-  }
+function isVoidElement(tagName) {
+  const voidElements = ['br', 'img', 'hr', 'input', 'meta'];
+  return voidElements.includes(tagName);
 }
 
-readHTMLFile('./JSweek02/스코프.html').then(function(htmlString) {
-  if (htmlString) {
-    var domTree = createDOMFromString(htmlString);
-    console.log(domTree);
-  }
-});
+const htmltoString = '<html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>HTML Parser Example</title></head><body><div><p>스코프와 실행컨텍스트</p></div></body></html>';
+const domTree = parseHTMLString(htmltoString);
 
-readHTMLFile('./JSweek02/실행컨텍스트.html').then(function(htmlString) {
-  if (htmlString) {
-    var domTree = createDOMFromString(htmlString);
-    console.log(domTree);
-  }
-});
+console.log(JSON.stringify(domTree, null, 2));
